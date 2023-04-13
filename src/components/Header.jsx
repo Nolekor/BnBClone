8c1723201971a14bd8eb1a5fd5bcf74229134126
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-13 21:05:25 +0200

    add static html and css of the site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const Header = () => {
+export const Header = () => {
   return (
     <div className="header-container">
       <img className="header-img" src="/airbnb-logo.png" alt="BnBLogo" />
     </div>
   );
 };
-
-export default Header;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <div className="hero-container">
+      <img
+        className="hero-img"
+        src="/photo-grid.png"
+        alt="lots of photo with some bnb expirience"
+      />
+      <h2 className="hero-title">Online Experience</h2>
+      <h4 className="hero-info">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </h4>
+    </div>
+  );
+};
