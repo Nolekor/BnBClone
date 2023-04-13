ffdd691887532020e67df7bab392f46f0c79c2fa
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-13 19:33:50 +0200

    Add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <img className="header-img" src="/airbnb-logo.png" alt="BnBLogo" />
+    </div>
+  );
+};
+
+export default Header;
